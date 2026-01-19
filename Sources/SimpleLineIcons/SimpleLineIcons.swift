b089c3d1854e868ee10f48eb550cd950bb4bf561
daironian <daironigr@gmail.com>
2026-01-19 17:51:54 +0100

    improve feat

--- Sources/SimpleLineIcons/SimpleLineIcons.swift
+++ Sources/SimpleLineIcons/SimpleLineIcons.swift
@@ -3,29 +3,28 @@
 import SwiftUI
 
 public struct SimpleLineIconSView: View {
-  private let icon: SimpleLineIcon
-  private let size: CGFloat
-  private let fontName: String
+    private let icon: SimpleLineIcon
+    private let size: CGFloat
+    private let fontName: String
 
-  public init(_ icon: SimpleLineIcon, size: CGFloat = 20, fontName: String = "Simple-Line-Icons") {
-    self.icon = icon
-    self.size = size
-    self.fontName = fontName
-  }
-    
-    public init(_ icon: String, size: CGFloat = 20, fontName: String = "Simple-Line-Icons") {
-      guard let iconEnum = SimpleLineIcon(name: icon) else {
-        fatalError("No matching case for \(icon)")
-      }
-      self.icon = iconEnum
-      self.size = size
-      self.fontName = fontName
+    public init(_ icon: SimpleLineIcon, size: CGFloat = 20, fontName: String = "Simple-Line-Icons") {
+        self.icon = icon
+        self.size = size
+        self.fontName = fontName
     }
 
-  public var body: some View {
-      Text(icon.unicodeScalar)
-          .font(.custom(fontName, size: size))
-  }
-    
-    
+    public init(_ icon: String, size: CGFloat = 20, fontName: String = "Simple-Line-Icons") {
+        if let iconEnum = SimpleLineIcon(name: icon) {
+            self.icon = iconEnum
+        } else {
+            self.icon = .simple_icon_info
+        }
+        self.size = size
+        self.fontName = fontName
+    }
+
+    public var body: some View {
+        Text(icon.unicodeScalar)
+            .font(.custom(fontName, size: size))
+    }
 }
